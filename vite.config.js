0b77101b16197f8104ba355204ea8007be9b0d66
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2025-08-02 20:32:59 -0700

    Clean up

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,60 @@
-import { observable, config } from '@observablehq/notebook-kit/vite';
-import { defineConfig } from 'vite';
+import { observable, config } from "@observablehq/notebook-kit/vite";
+import { defineConfig } from "vite";
+import { readFileSync, existsSync } from "node:fs";
+import Handlebars from "handlebars";
+import path from "node:path";
+import yaml from "yaml";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const TEMPLATE_PATH = path.join(__dirname, "src/template.html");
+
+const githubUrlBase =
+  "https://github.com/rreusser/notebooks/tree/main/notebooks";
 
 export default defineConfig({
   ...config(),
   plugins: [
-    observable({ })
+    observable({
+      template: TEMPLATE_PATH,
+      compileTemplate: function ({ template, input, context, notebook }) {
+        console.log(input);
+        const { path: sourcePath, filename } = context;
+        const dir = path.dirname(filename);
+        const metadataPath = path.join(dir, "metadata.yml");
+        const metadata = existsSync(metadataPath)
+          ? yaml.parse(readFileSync(metadataPath, "utf8"))
+          : {};
+        const sourceUrl = path.join(githubUrlBase, sourcePath);
+
+        if (notebook && notebook.cells.length) {
+          const titleCell = notebook.cells[0];
+          if (titleCell.value.startsWith("# ")) {
+            titleCell.value = [
+              titleCell.value + "\n",
+              "[Ricky Reusser](https://rreusser.github.io)",
+              metadata.publishedAt
+                ? new Date(metadata.publishedAt).toLocaleDateString("default", {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric",
+                  })
+                : "",
+              "[View source →](" + sourceUrl + ")",
+            ]
+              .filter(Boolean)
+              .join("<br>");
+          }
+        }
+
+        return Handlebars.compile(template)({
+          sourceUrl,
+          title: notebook.title,
+          ...metadata,
+        });
+      },
+    }),
   ],
-  root: 'notebooks',
-  base: '/',
+  root: "notebooks",
+  base: "/",
 });
