25d73c049ccf5f7fdbf76b3a1bcd82b2aff8a5ae
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2025-08-07 20:24:39 -0700

    Add BBP page

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { observable, config } from "@observablehq/notebook-kit/vite";
 import { defineConfig } from "vite";
 import { readFile } from "node:fs/promises";
 import Handlebars from "handlebars";
-import { join, dirname, resolve } from "node:path";
+import { join, dirname, resolve, relative } from "node:path";
 import yaml from "yaml";
 import { glob } from "glob";
 import { deserialize } from "@observablehq/notebook-kit";
@@ -14,6 +14,11 @@ const __dirname = dirname(new URL(import.meta.url).pathname);
 
 const TEMPLATE_PATH = join(__dirname, "lib/template.html");
 const GITHUB_BASE_URL = "https://github.com/rreusser/notebooks/tree/main/src";
+const NOTEBOOKS_DIR = join(__dirname, 'src');
+const notebookPaths = glob.sync(join(NOTEBOOKS_DIR, "**", "*.html"), {
+  nodir: true,
+  absolute: true,
+});
 
 async function readMetadata(filename) {
   let metadataYAML = "";
@@ -24,6 +29,21 @@ async function readMetadata(filename) {
   return yaml.parse(metadataYAML);
 }
 
+async function computeIndex() {
+  const notebooks = [];
+  for (const path of notebookPaths) {
+    const relpath = relative(NOTEBOOKS_DIR, path);
+    if (relpath === 'index.html') continue;
+    notebooks.push({
+      path: relpath,
+      ...await readMetadata(path)
+    });
+  }
+  notebooks.sort(({publishedAt: da}, {publishedAt: db}) => Date.parse(da || 0) - Date.parse(db || 0));
+  console.log(notebooks);
+  return notebooks;
+}
+
 export default defineConfig({
   ...config(),
   plugins: [
@@ -32,11 +52,23 @@ export default defineConfig({
       transformTemplate: async function (template, { filename, path }) {
         const notebook = deserialize(await readFile(filename, "utf8"), { parser });
         const metadata = await readMetadata(filename);
-        return Handlebars.compile(template)({
+        const isIndex = path === '/index.html';
+        const data = {
           sourceUrl: `${GITHUB_BASE_URL}/${path.replace(/^\//, '')}`,
+          author: 'Ricky Reusser',
+          authorUrl: 'https://rreusser.github.io',
           ...notebook,
           ...metadata,
-        });
+        };
+        if (isIndex) {
+          delete data.sourceUrl;
+          delete data.author;
+
+          // Add notebook links
+          data.index = await computeIndex();
+        }
+
+        return Handlebars.compile(template)(data);
       },
       transformNotebook: async function (notebook, { filename }) {
         // Remove the leading h1, preserving additional cell content, if present.
@@ -56,10 +88,7 @@ export default defineConfig({
     outDir: join(__dirname, "docs"),
     emptyOutDir: true,
     rollupOptions: {
-      input: glob.sync(join(__dirname, "src", "**", "*.html"), {
-        nodir: true,
-        absolute: true,
-      }),
+      input: notebookPaths
     },
   },
   root: "src",
