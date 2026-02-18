58a9b79cf8bf09557055697e697daebacf41905c
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2026-02-17 17:56:46 -0800

    rm

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ const S3_BASE = "https://s3.us-east-1.amazonaws.com/tilesets.rreusser.github.io"
 const tileUrls = {
   terrain: process.env.NODE_ENV === "production"
     ? `${S3_BASE}/denali-arcticdem-srtm30-v1/{z}/{x}/{y}.webp`
-    : "data/tiles/denali-arcticdem-srtm30-v1/{z}/{x}/{y}.webp",
+    : "data/tiles/denali-arcticdem-srtm30-v2/{z}/{x}/{y}.webp",
   sentinel: process.env.NODE_ENV === "production"
     ? `${S3_BASE}/denali-sentinel2-v1/{z}/{x}/{y}.webp`
     : "data/tiles/denali-sentinel2-v1/{z}/{x}/{y}.webp",
